0fdd0c871173789f24c63448259813efccbdfe88
ArturRubets <artur.rubets@gmail.com>
2022-08-03 17:33:38 +0300

    add map sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'map_sample/map_sample.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,49 +12,5 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Google Maps Demo',
       home: MapSample(),
     );
-  }
-}
-
-class MapSample extends StatefulWidget {
-  const MapSample({super.key});
-
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  final _controller = Completer<GoogleMapController>();
-
-  static const _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 4.4746,
-  );
-
-  static const _kLake = CameraPosition(
-    bearing: 192.8334901395799,
-    target: LatLng(37.43296265331129, -122.08832357078792),
-    tilt: 59.440717697143555,
-    zoom: 19.151926040649414,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: _controller.complete,
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final controller = await _controller.future;
-    return controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
